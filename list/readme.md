afcd276195e5f8dda5789371773fd389888d958a
frokhman <fajar.rokhman@verint.com>
2022-04-06 15:17:48 +0700

    a

---
+++ list/readme.md
@@ -0,0 +1,16 @@
+B64 adalah sebuah class untuk mengkonvert bilangan decimal ke bilangan berbasis 64.
+Bilangan berbasis 64 ini menggunakan karakter Base64 untuk merepresentasikan data. 
+
+Kegunaan dari class ini cukup banyak, antara lain untuk mengkompress data numeric dari data JSON.
+
+menggunakan decimal
+{
+    "data" : "1235478956"
+}
+
+menggunakan b64
+{
+    "data" : "BJo+ms"
+}
+
+Hasilnya lebih pendek
---
+++ list/src/List.ts
@@ -0,0 +1,119 @@
+
+namespace ha.list {
+
+    class List {
+        readonly ar: IListObj[] = [];
+        readonly obj: ListObj = new ListObj();
+        private aktif: IListObj;
+
+        getByNama(nama: string): IListObj {
+            for (let i: number = 0; i < this.ar.length; i++) {
+                if (this.ar[i].nama == nama) {
+                    this.aktif = this.ar[i];
+                    return this.aktif;
+                }
+            }
+
+            return null;
+        }
+
+        baru(nama: string): void {
+            this.ar.push({
+                nama: nama,
+                aktif: null
+            });
+        }
+
+        hapus(): void {
+            if (!this.aktif) {
+                return;
+            }
+
+            for (let i: number = 0; i < this.ar.length; i++) {
+                if (this.ar[i] == this.aktif) {
+                    this.ar.splice(i, 1);
+                    this.aktif = null;
+                    return;
+                }
+            }
+        }
+    }
+
+    class ListObj {
+        maju(list: IListObj) {
+            if (list.aktif.set) {
+                list.aktif = list.aktif.set;
+            }
+        }
+
+        mundur(list: IListObj) {
+            if (list.aktif.seb) {
+                list.aktif = list.aktif.seb;
+            }
+        }
+
+        sisip(list: IListObj, data: any): void {
+            if (!list.aktif) {
+                list.aktif = {
+                    data: data
+                }
+                return;
+            }
+
+            let item: IItem = {
+                seb: list.aktif,
+                set: list.aktif.set,
+                data: data
+            }
+
+            if (list.aktif.set) {
+                list.aktif.set.seb = item;
+            }
+
+            list.aktif.set = item;
+            list.aktif = item;
+        }
+
+        hapus(list: IListObj): void {
+            if (!list.aktif) {
+                return;
+            }
+
+            if (list.aktif.seb) {
+                list.aktif.seb.set = list.aktif.set;
+            }
+
+            if (list.aktif.set) {
+                list.aktif.set.seb = list.aktif.seb
+            }
+
+            if (list.aktif.seb) {
+                list.aktif = list.aktif.seb;
+            }
+            else if (list.aktif.set) {
+                list.aktif = list.aktif.set;
+            }
+            else {
+                list.aktif = null;
+            }
+        }
+
+        update(list: IListObj, data: any): void {
+            if (!list.aktif) return;
+            list.aktif.data = data;
+        }
+    }
+
+    interface IListObj {
+        nama?: string,
+        aktif?: IItem
+    }
+
+    interface IItem {
+        seb?: IItem
+        set?: IItem
+        data: any
+    }
+
+    export var list: List = new List();
+}
