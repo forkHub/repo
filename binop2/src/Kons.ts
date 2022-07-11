7fa56a6a4d243c1ced4b52d662412fa8aa287af1
frokhman <fajar.rokhman@verint.com>
2022-07-11 21:20:30 +0700

    bersih2

--- binop2/src/Kons.ts
+++
@@ -1,7 +0,0 @@
-namespace ha.binop {
-    export class Kons {
-        static readonly TY_ANGKA: string = 'angka';
-        static readonly TY_TEKS: string = 'teks';
-        static readonly TY_OPR: string = 'operator';
-    }
-}
--- binop4/src/Index.ts
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    md.init().catch((e) => {
-        console.error(e);
-    })
-}
