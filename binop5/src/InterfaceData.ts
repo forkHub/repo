707c3a4911639785776dad110cf370aec0fb5b8c
frokhman <fajar.rokhman@verint.com>
2022-08-04 06:22:46 +0700

    bersih

--- binop5/src/InterfaceData.ts
+++
@@ -1,7 +0,0 @@
-interface IData {
-	id: number
-	indukId: number
-	type: string
-	nama: string
-	ket: string
-}
--- binop5/src/OpEditor.ts
+++
@@ -1,9 +0,0 @@
-class OpEditor extends ha.comp.BaseComponent {
-    constructor() {
-        super();
-        this._template = `
-        `;
-
-    }
-}
-const opEditor: OpEditor = new OpEditor();
