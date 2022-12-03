27e90c5f1926d95383b53da360c0afaff970f23e
frokhman <fajar.rokhman@verint.com>
2022-12-03 17:31:57 +0700

    update baca js

--- blitz/bacajs/src/baca.ts
+++ blitz/bacajs/src/baca.ts
@@ -4,12 +4,19 @@ let file: string = process.argv.slice(2)[0];
 
 let buff: string = fs.readFileSync(file, 'utf-8');
 let hasil: string = '';
+// let statusEnter: boolean = false;
+let statusKomentSekarang: boolean = false;
+let statusKomentSebelum: boolean = false;
 
-//buff.split(/\r?\n/).forEach((str: string) => {
 buff.split(/\n/).forEach((str: string) => {
+	updateStatusKomentar(str);
 
 	if (tambahEnterSebelum(str)) {
 		hasil += '\n';
+		// statusEnter = true; 
+	}
+	else {
+		// statusEnter = false;
 	}
 
 	hasil += str;
@@ -35,12 +42,33 @@ function tambahEnterSesudah(str: string): boolean {
 	return false;
 }
 
+function updateStatusKomentar(str: string): void {
+	statusKomentSebelum = statusKomentSekarang;
+
+	if (str.trim().slice(0, 2) == '//') {
+		statusKomentSekarang = true;
+	}
+	else {
+		statusKomentSekarang = false;
+	}
+
+	console.log('update status komentar');
+	console.log('str ' + str);
+	console.log('sebelum ' + statusKomentSebelum);
+	console.log('sekarang ' + statusKomentSekarang);
+	console.log('');
+}
+
 function tambahEnterSebelum(str: string): boolean {
-	if (str.indexOf('function') > -1) return true;
-	if (str.indexOf('if (') > -1) return true;
-	if (str.indexOf('else if (') > -1) return true;
-	if (str.indexOf('else {') > -1) return true;
-	if (str.indexOf('for (let') > -1) return true;
+	if (statusKomentSebelum == false) {
+		if (str.indexOf('//') > -1) return true;
+		if (str.indexOf('/*') > -1) return true;
+		if (str.indexOf('function') > -1) return true;
+		if (str.indexOf('if (') > -1) return true;
+		if (str.indexOf('else if (') > -1) return true;
+		if (str.indexOf('else {') > -1) return true;
+		if (str.indexOf('for (let') > -1) return true;
+	}
 
 	return false;
 }
--- blitz/bacajs/target/baca.js
+++ blitz/bacajs/target/baca.js
@@ -7,9 +7,17 @@ const fs_1 = __importDefault(require("fs"));
 let file = process.argv.slice(2)[0];
 let buff = fs_1.default.readFileSync(file, 'utf-8');
 let hasil = '';
-buff.split(/\r?\n/).forEach((str) => {
+// let statusEnter: boolean = false;
+let statusKomentSekarang = false;
+let statusKomentSebelum = false;
+buff.split(/\n/).forEach((str) => {
+    updateStatusKomentar(str);
     if (tambahEnterSebelum(str)) {
         hasil += '\n';
+        // statusEnter = true; 
+    }
+    else {
+        // statusEnter = false;
     }
     hasil += str;
     hasil += '\n';
@@ -17,6 +25,11 @@ buff.split(/\r?\n/).forEach((str) => {
         hasil += '\n';
     }
 });
+let ctr = 0;
+while (ctr < 100) {
+    ctr++;
+    // hasil.replace(/\r?\n\r?\n\r?\n/, '\n');
+}
 //tulis ke file
 fs_1.default.writeFileSync(process.argv.slice(2)[1], hasil);
 function tambahEnterSesudah(str) {
@@ -24,16 +37,36 @@ function tambahEnterSesudah(str) {
         return true;
     return false;
 }
+function updateStatusKomentar(str) {
+    statusKomentSebelum = statusKomentSekarang;
+    if (str.trim().slice(0, 2) == '//') {
+        statusKomentSekarang = true;
+    }
+    else {
+        statusKomentSekarang = false;
+    }
+    console.log('update status komentar');
+    console.log('str ' + str);
+    console.log('sebelum ' + statusKomentSebelum);
+    console.log('sekarang ' + statusKomentSekarang);
+    console.log('');
+}
 function tambahEnterSebelum(str) {
-    if (str.indexOf('function') > -1)
-        return true;
-    if (str.indexOf('if (') > -1)
-        return true;
-    if (str.indexOf('else if (') > -1)
-        return true;
-    if (str.indexOf('else {') > -1)
-        return true;
-    if (str.indexOf('for (let') > -1)
-        return true;
+    if (statusKomentSebelum == false) {
+        if (str.indexOf('//') > -1)
+            return true;
+        if (str.indexOf('/*') > -1)
+            return true;
+        if (str.indexOf('function') > -1)
+            return true;
+        if (str.indexOf('if (') > -1)
+            return true;
+        if (str.indexOf('else if (') > -1)
+            return true;
+        if (str.indexOf('else {') > -1)
+            return true;
+        if (str.indexOf('for (let') > -1)
+            return true;
+    }
     return false;
 }
