979780bffde9ffc2b58a194ae6960508d5f07aa4
frokhman <fajar.rokhman@verint.com>
2023-03-26 13:51:25 +0700

    moon3

---
+++ moon3/path_finder/Point.ts
@@ -0,0 +1,14 @@
+namespace fg {
+    export class Point {
+        x: number;
+        y: number;
+
+        constructor(i?: number, j?: number) {
+            this.x = i;
+            this.y = j;
+        }
+
+    }
+
+
+}
