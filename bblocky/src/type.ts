d7564487bf4b921df5dedc317d7a5437934e4616
frokhman <fajar.rokhman@verint.com>
2023-08-20 17:54:49 +0700

    update command

--- bblocky/src/type.ts
+++
@@ -1,8 +0,0 @@
-type TBlockly = {
-    inject: (p: any, p2: any) => void;
-    Xml: {
-        domToWorkspace: (workspaceBlocks: any, workspace: any) => void;
-    }
-}
-
-declare var Blockly: TBlockly;
