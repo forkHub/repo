5f9f22285d580e6ec6fcf34bebb85c52175029cf
frokhman <fajar.rokhman@verint.com>
2023-12-16 20:27:17 +0700

    add dlg temp disable export-import

--- bblocky/editor/src/Dialog.ts
+++ bblocky/editor/src/Dialog.ts
@@ -1,1 +1,15 @@
-//TODO:
+namespace ha.be {
+    export class Dialog {
+        private static dlg: HTMLDialogElement;
+
+        static show(msg: string) {
+            this.dlg = document.body.querySelector("dialog.alert") as HTMLDialogElement;
+            this.dlg.querySelector('P').innerHTML = msg;
+            (this.dlg as any).showModal();
+        }
+
+        static klik() {
+            (this.dlg as any).close();
+        }
+    }
+}
